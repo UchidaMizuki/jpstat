0d0dff113c942fee9154547461ba0b8045919350
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-27 11:25:35 +0900

    Update logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -1,14 +1,14 @@
 library(tidyverse)
-library(fs)
+library(jpgrid)
 library(hexSticker)
 
 pkgload::load_all()
 
-font_logo_en <- "Poppins"
-font_logo_ja <- "Noto Sans JP"
+theme_set(theme_void())
 
-sysfonts::font_add_google(font_logo_en)
-sysfonts::font_add_google(font_logo_ja)
+font_logo <- "Poppins"
+
+sysfonts::font_add_google(font_logo)
 
 # logo --------------------------------------------------------------------
 
@@ -17,80 +17,95 @@ file_logo <- "man/figures/logo.png"
 fill_logo <- "#BC002D"
 color_logo <- "snow"
 
-pop_census_2020 <- estat(appId = keyring::key_get("estat-api"),
-                         statsDataId = "0003445133",
-                         lang = "E")
+JGD2011 <- 6668
 
-pop_census_2020 <- pop_census_2020 |>
+appId <- keyring::key_get("estat-api")
+
+# Population
+pop_2015 <- estat(appId = appId,
+                  statsDataId = "0000020201",
+                  lang = "E")
+
+pop_2015 <- pop_2015 |>
   activate(tab) |>
   select() |>
 
   activate(cat01) |>
-  filter(name == "Total") |>
+  filter(name == "A1101_Total population (Both sexes)") |>
   select() |>
-
-  activate(cat02) |>
-  rekey("sex") |>
-  filter(name %in% c("Male", "Female")) |>
-  select(name) |>
-
-  activate(cat03) |>
-  rekey("age") |>
-  select(name) |>
 
   activate(area) |>
-  filter(name == "Japan") |>
-  select() |>
+  rekey("city") |>
+  select(code, name) |>
 
   activate(time) |>
+  filter(name == "2015") |>
   select() |>
 
-  collect(n = "pop")
+  collect(n = "pop") |>
+  mutate(pop = parse_number(pop))
 
-pop_census_2020 <- pop_census_2020 |>
-  filter(str_detect(age_name, "^\\d+ years old")) |>
-  mutate(sex = sex_name |>
-           str_to_lower() |>
-           as_factor(),
-         age = age_name |>
-           str_extract("^\\d+") |>
-           as.integer(),
-         pop = parse_number(pop)) |>
-  select(sex, age, pop)
+# habitable_area
+habitable_area_2015 <- estat(appId = appId,
+                             statsDataId = "0000020202",
+                             lang = "E")
 
-plot_pop_census_2020 <- pop_census_2020 |>
-  mutate(pop = case_when(sex == "male" ~ -pop,
-                         sex == "female" ~ pop)) |>
-  add_row(sex = factor(c("male", "female")),
-          age = -1,
-          pop = 0) |>
-  ggplot(aes(age, pop,
-             fill = sex)) +
-  geom_polygon(show.legend = FALSE) +
-  geom_hline(yintercept = 0,
-             color = fill_logo,
-             linewidth = 1) +
-  scale_y_continuous(limits = c(-max(pop_census_2020$pop) - 1, max(pop_census_2020$pop) + 1)) +
-  scale_fill_manual(values = c(male = "cornflowerblue",
-                               female = "lightcoral")) +
-  coord_flip() +
-  theme(plot.margin = margin(r = 10,
-                             l = 10))
+habitable_area_2015 <- habitable_area_2015 |>
+  activate(tab) |>
+  select() |>
 
-sticker(plot_pop_census_2020,
-        package = "jpstat",
+  activate(cat01) |>
+  filter(name == "B1103_Inhabitable area") |>
+  select() |>
+
+  activate(area) |>
+  rekey("city") |>
+  select(code, name) |>
+
+  activate(time) |>
+  filter(name == "2015") |>
+
+  collect(n = "habitable_area_ha") |>
+  mutate(habitable_area_ha = parse_number(habitable_area_ha,
+                                          na = "-"))
+
+popdens_2015 <- grid_city2015 |>
+  group_by(city_code) |>
+  mutate(size_city_code = n()) |>
+  inner_join(pop_2015 |>
+               select(city_code, pop),
+             by = "city_code") |>
+  inner_join(habitable_area_2015 |>
+               select(city_code, habitable_area_ha),
+             by = "city_code") |>
+  mutate(across(c(pop, habitable_area_ha),
+                ~ .x / size_city_code)) |>
+  select(!size_city_code) |>
+
+  mutate(grid = grid_80km(grid)) |>
+  group_by(grid) |>
+  summarise(across(c(pop, habitable_area_ha),
+                   partial(sum,
+                           na.rm = TRUE))) |>
+  mutate(popdens_per_ha = pop / habitable_area_ha) |>
+  select(!c(pop, habitable_area_ha)) |>
+  sf::st_as_sf(crs = JGD2011)
+
+plot_popdens_2015 <- popdens_2015 |>
+  ggplot(aes(fill = popdens_per_ha)) +
+  geom_sf(show.legend = FALSE,
+          color = fill_logo) +
+  scale_fill_viridis_c(option = "turbo",
+                       trans = "log10")
+
+sticker(plot_popdens_2015,
+        package = "",
         filename = file_logo,
 
-        s_width = 1.7,
-        s_height = 0.95,
+        s_width = 1.6,
+        s_height = 1.6,
         s_x = 1,
-        s_y = 1.35,
-
-        p_size = 30,
-        p_color = color_logo,
-        p_y = 0.65,
-        p_family = font_logo_en,
-        p_fontface = "bold.italic",
+        s_y = 1,
 
         h_fill = fill_logo,
         h_color = "transparent",
@@ -100,13 +115,15 @@ sticker(plot_pop_census_2020,
         l_y = 1,
         l_width = 6,
         l_height = 6) +
-  geom_url(url = "ジェーピースタット",
-           family = font_logo_ja,
-           fontface = "bold",
-           size = 8.5,
+  geom_url(url = "jpstat",
+           x = 0.975,
+           y = 0.225,
+           family = font_logo,
+           fontface = "bold.italic",
+           size = 22,
            color = color_logo) +
-  theme(plot.margin = margin(r = -1.5,
-                             l = -1.5))
+  theme(plot.margin = margin(r = -1,
+                             l = -1))
 
 save_sticker(file_logo)
 usethis::use_logo(file_logo)
