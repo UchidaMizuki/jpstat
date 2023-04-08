7c4e837029a0912090e3f1cffd6a97bb6c520e43
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-04-08 10:52:33 +0900

    Update logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -13,73 +13,35 @@ sysfonts::font_add_google(font_logo)
 
 file_logo <- "man/figures/logo.png"
 
-fill_logo <- "#BC002D"
+fill_logo <- "firebrick"
 color_logo <- "snow"
 
 JGD2011 <- 6668
 
-appId <- keyring::key_get("estat-api")
+plot_grid_city <- grid_city |>
+  mutate(grid = grid_convert(grid, "80km")) |>
+  distinct(grid) |>
+  grid_as_sf(crs = JGD2011) |>
 
-# Population
-popdens_2015 <- estat(appId = appId,
-                      statsDataId = "0000010201",
-                      lang = "E")
+  mutate(fill = if_else(as.character(grid) == "3653",
+                        "fill_logo",
+                        "color_logo")) |>
 
-popdens_2015 <- popdens_2015 |>
-  activate(tab) |>
-  select() |>
+  ggplot() +
+  geom_sf(aes(fill = fill),
+          show.legend = FALSE,
+          color = fill_logo) +
+  scale_fill_manual(values = c(fill_logo = fill_logo,
+                               color_logo = color_logo))
 
-  activate(cat01) |>
-  filter(name == "#A01202_Population per 1 km2 of inhabitable area") |>
-  select() |>
-
-  activate(area) |>
-  rekey("pref") |>
-  filter(name != "All Japan") |>
-  select(code) |>
-
-  activate(time) |>
-  filter(name == "2015") |>
-  select() |>
-
-  collect(n = "popdens_per_1km2") |>
-  mutate(pref_code = pref_code |>
-           str_extract("^\\d{2}") |>
-           as.integer(),
-         popdens_per_1km2 = parse_number(popdens_per_1km2))
-
-japan <- rnaturalearth::ne_states("japan",
-                                  returnclass = "sf") |>
-  select(iso_3166_2, name) |>
-  mutate(pref_code = iso_3166_2 |>
-           str_extract("(?<=JP-)\\d{2}") |>
-           as.integer()) |>
-  select(!iso_3166_2)
-
-# bbox_japan <- grid_city2015 |>
-#   mutate(grid = grid_80km(grid)) |>
-#   distinct(grid) |>
-#   sf::st_bbox()
-
-plot_popdens_2015 <- japan |>
-  left_join(popdens_2015,
-            by = "pref_code") |>
-  ggplot(aes(fill = popdens_per_1km2)) +
-  geom_sf(show.legend = FALSE,
-          color = "transparent") +
-  scale_fill_viridis_c(option = "turbo",
-                       trans = "log10")
-  # coord_sf(xlim = bbox_japan[c("xmin", "xmax")],
-  #          ylim = bbox_japan[c("ymin", "ymax")])
-
-sticker(plot_popdens_2015,
+sticker(plot_grid_city,
         package = "",
         filename = file_logo,
 
-        s_width = 2.0,
-        s_height = 2.0,
+        s_width = 1.6,
+        s_height = 1.6,
         s_x = 1,
-        s_y = 0.9,
+        s_y = 1,
 
         h_fill = fill_logo,
         h_color = "transparent") +
@@ -90,8 +52,79 @@ sticker(plot_popdens_2015,
            fontface = "bold.italic",
            size = 22,
            color = color_logo) +
-  theme(plot.margin = margin(r = -1,
-                             l = -1))
+  theme(plot.margin = margin(r = -1.5,
+                             l = -1.5))
 
 save_sticker(file_logo)
 usethis::use_logo(file_logo)
+
+# appId <- keyring::key_get("estat-api")
+#
+# # Population
+# popdens_2015 <- estat(appId = appId,
+#                       statsDataId = "0000010201",
+#                       lang = "E")
+#
+# popdens_2015 <- popdens_2015 |>
+#   activate(tab) |>
+#   select() |>
+#
+#   activate(cat01) |>
+#   filter(name == "#A01202_Population per 1 km2 of inhabitable area") |>
+#   select() |>
+#
+#   activate(area) |>
+#   rekey("pref") |>
+#   filter(name != "All Japan") |>
+#   select(code) |>
+#
+#   activate(time) |>
+#   filter(name == "2015") |>
+#   select() |>
+#
+#   collect(n = "popdens_per_1km2") |>
+#   mutate(pref_code = pref_code |>
+#            str_extract("^\\d{2}") |>
+#            as.integer(),
+#          popdens_per_1km2 = parse_number(popdens_per_1km2))
+#
+# japan <- rnaturalearth::ne_states("japan",
+#                                   returnclass = "sf") |>
+#   select(iso_3166_2, name) |>
+#   mutate(pref_code = iso_3166_2 |>
+#            str_extract("(?<=JP-)\\d{2}") |>
+#            as.integer()) |>
+#   select(!iso_3166_2)
+#
+# plot_popdens_2015 <- japan |>
+#   left_join(popdens_2015,
+#             by = "pref_code") |>
+#   ggplot(aes(fill = popdens_per_1km2)) +
+#   geom_sf(show.legend = FALSE,
+#           color = "transparent") +
+#   scale_fill_viridis_c(option = "turbo",
+#                        trans = "log10")
+#
+# sticker(plot_popdens_2015,
+#         package = "",
+#         filename = file_logo,
+#
+#         s_width = 2.0,
+#         s_height = 2.0,
+#         s_x = 1,
+#         s_y = 0.9,
+#
+#         h_fill = fill_logo,
+#         h_color = "transparent") +
+#   geom_url(url = "jpstat",
+#            x = 0.975,
+#            y = 0.225,
+#            family = font_logo,
+#            fontface = "bold.italic",
+#            size = 22,
+#            color = color_logo) +
+#   theme(plot.margin = margin(r = -1,
+#                              l = -1))
+#
+# save_sticker(file_logo)
+# usethis::use_logo(file_logo)
